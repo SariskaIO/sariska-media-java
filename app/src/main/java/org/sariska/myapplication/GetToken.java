20519cc32b2b2617747c13d1533642b65d8b8839
DIPAK KUMAR SISODIYA <dipak@sariska.io>
2022-07-07 23:07:15 +0530

    API key change

--- app/src/main/java/org/sariska/myapplication/GetToken.java
+++ app/src/main/java/org/sariska/myapplication/GetToken.java
@@ -19,7 +19,7 @@ public class GetToken {
         final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
         String url = "https://api.sariska.io/api/v1/misc/generate-token";
         String json = "{\n" +
-                "    \"apiKey\": \"27fd6f8080d512442a3694f461adb3986cda5ba39dbe368d75\",\n" +
+                "    \"apiKey\": \"249202aabed00b41363794b526eee6927bd35cbc9bac36cd3edcaa\",\n" +
                 "    \"user\": {\n" +
                 "        \"id\": \"dsds\",\n" +
                 "        \"name\": \"SomeOne\",\n" +
